bb0741fb3b7b82f3b77ca24a1304b935766839f9
Ho Wa Wong <howa.wong@gmail.com>
2019-08-25 01:53:25 +0800

    Update LinkReport

--- api/report/models.py
+++ api/report/models.py
@@ -3,6 +3,8 @@ from django.core.files.storage import default_storage
 from enum import Enum
 import imagehash
 from PIL import Image
+from url_normalize import url_normalize
+from goose3 import Goose
 # Create your models here.
 
 ReportTypes = (
@@ -23,7 +25,21 @@ class ImageReport(models.Model):
 
 class LinkReport(models.Model):
     url = models.CharField(max_length=2048)
+    title = models.CharField(max_length=1024, blank=True)
+    text = models.CharField(max_length=4096, blank=True)
+    short_text = models.CharField(max_length=4096, blank=True)
+    image = models.CharField(max_length=4096, blank=True)
 
+
+    def save(self, *args, **kwargs):
+        self.url = url_normalize(self.url)
+        g = Goose()
+        article = g.extract(url=self.url)
+        self.title = article.title
+        self.short_text = article.meta_description
+        self.text = article.cleaned_text
+        self.image = article.opengraph.get('image', '') if article.top_image is None else article.top_image.src
+        super(LinkReport, self).save(*args, **kwargs)
 
 class Report(models.Model):
     report_type = models.CharField(max_length=128, choices=ReportTypes)
